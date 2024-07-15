db36663dc6c3923186c9866a4ce8ae93d04b2210
tada114 <165593628+tada114@users.noreply.github.com>
2024-07-16 00:15:27 +0800

    NAU-6566 update REAMDE.md add example (#10)
    * NAU-6566 update REAMDE.md add example
    
    * NAU-6566 update REAMDE.md add example

--- client/create_token.go
+++ client/create_token.go
@@ -7,9 +7,14 @@ import (
 )
 
 // CreateToken Create token for tenant that will be used to authenticate the tenant in the system
+// You need tenant token in case you want to call API with token instead of API key
+// That case when you want to pass token to client or another service to call other APIs directly
 func (s *Client) CreateToken(ctx context.Context) (*TokenResponse, error) {
 	var resp *TokenResponse
-	err := s.client.Send(ctx, fmt.Sprintf("%s%s", s.domain, EndpointGetToken), s.BuildHeader(""), "", http.MethodPost, &resp)
+	err := s.client.Send(
+		ctx, fmt.Sprintf("%s%s", s.domain, EndpointGetToken),
+		s.BuildHeader(), "", http.MethodPost, &resp,
+	)
 	if err != nil {
 		return nil, err
 	}
